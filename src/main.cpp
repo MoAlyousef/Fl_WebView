b080d7cfa1eb5b73ee67ae593b39142862636e48
Mohammed Alyousef <may642_2000@hotmail.com>
2021-05-24 23:00:13 +0300

    fix macos

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,14 @@ public:
     gdk_win_xid = GDK_WINDOW_XID(gdk_win);
     Fl::add_idle(+[](void *) { gtk_main_iteration(); });
   }
-  virtual void draw() override {
+  void init() {
     auto wv_win_xid = fl_xid(this);
     auto main_win_xid = fl_xid(this->top_window());
     XMapWindow(fl_display, gdk_win_xid);
     XReparentWindow(fl_display, gdk_win_xid, main_win_xid, x(), y());
     XFlush(fl_display);
-    webview_set_size(wv, w(), h(), 0);
   }
+  virtual void draw() override { webview_set_size(wv, w(), h(), 0); }
   void navigate(const char *addr) { webview_navigate(wv, addr); }
 };
 
@@ -38,16 +38,20 @@ public:
 #include <CoreGraphics/CoreGraphics.h>
 #include <objc/objc-runtime.h>
 
-extern "C" void reparent(void *, void *);
+extern "C" void helper_reparent(void *child, void *parent);
 
 class Fl_WebView : public Fl_Window {
   webview_t wv = NULL;
+
 public:
   Fl_WebView(int x, int y, int w, int h, const char *title = 0)
       : Fl_Window(x, y, w, h, title) {
     wv = webview_create(false, NULL);
-    reparent((void *)webview_get_window(wv), (void *)fl_xid(this));
-    webview_set_size(wv, w, h, 0);
+  }
+  void init() {
+    webview_set_size(wv, w(), h(), 0);
+    helper_reparent((void *)webview_get_window(wv), (void *)fl_xid(this));
+    Fl::add_idle(webview_run, wv);
   }
   virtual void draw() override { webview_set_size(wv, w(), h(), 0); }
   void navigate(const char *addr) { webview_navigate(wv, addr); }
@@ -64,6 +68,7 @@ public:
       : Fl_Window(x, y, w, h, title) {
     wv = webview_create(false, (HWND *)fl_xid(this));
   }
+  void init() {}
   virtual void draw() override { webview_set_size(wv, w(), h(), 0); }
   void navigate(const char *addr) { webview_navigate(wv, addr); }
 };
@@ -75,8 +80,13 @@ int main(int argc, char **argv) {
   auto main_win = new Fl_Window(800, 600);
   auto wv_win = new Fl_WebView(5, 5, 790, 590);
   main_win->end();
+  main_win->resizable(main_win);
   main_win->show();
+  main_win->callback(+[](Fl_Widget *, void *data){
+    if (Fl::event() == FL_CLOSE) std::exit(0);
+  });
 
+  wv_win->init();
   wv_win->navigate("https://google.com");
 
   return Fl::run();
