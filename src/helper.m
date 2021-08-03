347c2dc15244b9b58442daf54b857f0825c5cfc2
Mohammed Alyousef <may642_2000@hotmail.com>
2021-08-03 15:21:01 +0300

    fix macos build

--- src/helper.m
+++ src/helper.m
@@ -1,8 +1,8 @@
 #import <Cocoa/Cocoa.h>
 
-void helper_reparent(void *child_win, void *parent_win) {
-    NSWindow *parent = (NSWindow *)parent;
-    NSWindow *child = (NSWindow *)child;
+void make_delegate(void *child_win, void *parent_win) {
+    NSWindow *parent = (NSWindow *)parent_win;
+    NSWindow *child = (NSWindow *)child_win;
     [parent setDelegate:(id)child];
     [child makeKeyAndOrderFront:nil];
 }
