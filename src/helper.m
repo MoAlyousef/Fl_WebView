b0cf9888b51ba1eebe9e4a3a38a147651727b5d0
Mohammed Alyousef <may642_2000@hotmail.com>
2021-08-03 15:27:38 +0300

    fix event handling on macos

--- src/helper.m
+++ src/helper.m
@@ -5,4 +5,6 @@ void make_delegate(void *child_win, void *parent_win) {
     NSWindow *child = (NSWindow *)child_win;
     [parent setDelegate:(id)child];
     [child makeKeyAndOrderFront:nil];
+    [child orderWindow:NSWindowAbove relativeTo:[parent windowNumber]];
+    [child setIgnoresMouseEvents:NO];
 }
