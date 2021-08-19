20cf987dc48f7cedd17a1800e927d215f9697303
Mohammed Alyousef <may642_2000@hotmail.com>
2021-08-19 00:07:02 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 #include "webview.h"
 #include <FL/Enumerations.H>
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
 #include <FL/platform.H>
 #include <stdexcept>
 
 #if defined(__linux__)
 #include <X11/Xlib.h>
+#include <X11/extensions/Xfixes.h>
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 #include <glib.h>
@@ -17,35 +18,64 @@ int check(void *) {
   return Fl::check();
 }
 
-class Fl_WebView : public Fl_Window {
+void x_init(Display *disp, Window child, Window parent) {
+  XMoveWindow(disp, child, 0, 0);
+
+  XReparentWindow(disp, child, parent, 0, 0);
+
+  XFixesChangeSaveSet(disp, child, SetModeInsert, SaveSetRoot,
+                      SaveSetUnmap);
+
+  XEvent client_event;
+  XWindowAttributes childAttributes;
+  XWindowAttributes parentAttributes;
+  XGetWindowAttributes(disp, child, &childAttributes);
+  XGetWindowAttributes(disp, parent, &parentAttributes);
+
+  client_event.type = ConfigureNotify;
+  client_event.xconfigure.send_event = True;
+  client_event.xconfigure.display = disp;
+  client_event.xconfigure.event = child;
+  client_event.xconfigure.window = child;
+  client_event.xconfigure.width = childAttributes.width;
+  client_event.xconfigure.height = childAttributes.height;
+  client_event.xconfigure.border_width = 0;
+  client_event.xconfigure.above = None;
+  client_event.xconfigure.override_redirect = True;
+
+  XSendEvent(disp, child, False, StructureNotifyMask, &client_event);
+}
+
+class Fl_WebView : public Fl_Double_Window {
   webview_t wv = NULL;
   long gdk_win_xid = 0;
   bool is_gnome = false;
 
 public:
   Fl_WebView(int x, int y, int w, int h, const char *title = 0)
-      : Fl_Window(x, y, w, h, title) {
+      : Fl_Double_Window(x, y, w, h, title) {
     wv = webview_create(false, NULL);
     auto wv_gtk_win = webview_get_window(wv);
     auto gdk_win = gtk_widget_get_window(GTK_WIDGET(wv_gtk_win));
     gdk_win_xid = GDK_WINDOW_XID(gdk_win);
-    Fl_Window::end();
+    Fl_Double_Window::end();
     // doesn't have gnome-shell
-    // TODO: Check WM_NAME thru X11
-    is_gnome = !system("gnome-shell --help");
+    // TODO: Check WM_NAME thru Xlib
+    is_gnome = !system("gnome-shell --help > /dev/null");
   }
   void init() {
     if (!shown())
       throw std::runtime_error("The window needs to be shown.");
     if (!is_gnome) {
-      XReparentWindow(fl_display, gdk_win_xid, fl_xid(this), 0, 0);
+      x_init(fl_display, gdk_win_xid, fl_xid(this));
     }
+    this->flush();
     g_idle_add(check, NULL);
     Fl::add_idle(+[](void *) { gtk_main(); });
   }
   virtual void draw() override {
     if (is_gnome) {
-      XReparentWindow(fl_display, gdk_win_xid, fl_xid(this), 0, 0);
+      x_init(fl_display, gdk_win_xid, fl_xid(this));
       usleep(3e5);
     }
     webview_set_size(wv, w(), h(), 0);
@@ -57,13 +87,13 @@ public:
 
 extern "C" void make_delegate(void *child, void *parent);
 
-class Fl_WebView : public Fl_Window {
+class Fl_WebView : public Fl_Double_Window {
   webview_t wv = NULL;
 
 public:
   Fl_WebView(int x, int y, int w, int h, const char *title = 0)
-      : Fl_Window(x, y, w, h, title) {
-    Fl_Window::end();
+      : Fl_Double_Window(x, y, w, h, title) {
+    Fl_Double_Window::end();
   }
   void init() {
     if (!shown())
@@ -80,13 +110,13 @@ public:
 #elif defined(_WIN32)
 #include <windows.h>
 
-class Fl_WebView : public Fl_Window {
+class Fl_WebView : public Fl_Double_Window {
   webview_t wv = NULL;
 
 public:
   Fl_WebView(int x, int y, int w, int h, const char *title = 0)
-      : Fl_Window(x, y, w, h, title) {
-    Fl_Window::end();
+      : Fl_Double_Window(x, y, w, h, title) {
+    Fl_Double_Window::end();
   }
   void init() {
     if (!shown())
@@ -101,7 +131,7 @@ public:
 #endif
 
 int main(int argc, char **argv) {
-  auto main_win = new Fl_Window(800, 600);
+  auto main_win = new Fl_Double_Window(800, 600);
   auto wv_win = new Fl_WebView(5, 5, 790, 590);
   main_win->end();
   main_win->resizable(main_win);
